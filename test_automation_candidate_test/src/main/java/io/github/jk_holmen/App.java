02e18f151e0be67c2094dd9d9d4cc1309c110f65
jk-holmen <166861650+jk-holmen@users.noreply.github.com>
2026-03-11 21:56:37 +0100

    Generate Maven project from archetype and update versions

---
+++ test_automation_candidate_test/src/main/java/io/github/jk_holmen/App.java
@@ -0,0 +1,13 @@
+package io.github.jk_holmen;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
